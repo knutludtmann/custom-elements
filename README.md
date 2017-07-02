0f7da6257f3c00e66a5ebd850d86aca704f4e293
Knut Ludtmann <mail@knut-ludtmann.de>
2017-07-02 20:55:52 +0200

    native web component tab layout

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# Frontend Setup 
++ webpack 2 
++ PostCSS 
++ cssnext 
++ sourcemaps 
++ browsersync 
++ lost
++ stylelint https://github.com/JaKXz/stylelint-webpack-plugin 
++ .stylelintrc https://stylelint.io/user-guide/rules/
+
+
+### Start: PostCSS + cssnext
+https://blog.madewithenvy.com/webpack-2-postcss-cssnext-fdcd2fd7d0bd
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path'),
+    webpack = require('webpack'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin'),
+    BrowserSyncPlugin = require('browser-sync-webpack-plugin'),
+    lost = require('lost'),
+    StyleLintPlugin = require('stylelint-webpack-plugin'),
+    html = require('html-loader');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        app: './app.js'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            exclude: ['/node_modules/'],
+            use: ExtractTextPlugin.extract({
+                /* fallbackLoader: 'style-loader', //importLoaders=1? */
+                loader: [{
+                        loader: 'css-loader',
+                        query: { sourceMap: true }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        query: { sourceMap: true }
+                    }
+                ],
+            }),
+        },
+        {
+            test: /\.html$/,
+            use: [ {
+            loader: 'html-loader',
+            options: {
+                minimize: true
+            }
+            }],
+        } ],
+    },
+    plugins: [
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3006,
+            server: { baseDir: ['./'] }
+        }),
+        new ExtractTextPlugin('[name].bundle.css'),
+        new StyleLintPlugin({
+            configFile: '.stylelintrc',
+            context: 'src',
+            files: '**/*.css',
+            failOnError: false,
+            quiet: false,
+        })
+
+    ],
+
+    devtool: 'source-map',
+    watch: true,
+    output: {
+        path: path.resolve(__dirname, 'dist/assets'),
+        filename: '[name].bundle.css'
+            /*,
+                    sourceMapFilename: '[file].map'*/
+    }
+}
